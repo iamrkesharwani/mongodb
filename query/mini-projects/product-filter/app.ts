38f3b58ffd9f7b754c56fcd444af3e54c754138a
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-03-16 20:39:04 +0530

    add query phase

--- query/mini-projects/product-filter/app.ts
+++ query/mini-projects/product-filter/app.ts
@@ -7,5 +7,33 @@ export async function filterProducts(
 ) {
   const query: Filter<Product> = {};
 
-  
+  if (filters.categories && filters.categories.length > 0) {
+    query.category = { $in: filters.categories } as any;
+  }
+
+  if (filters.minPrice !== undefined || filters.maxPrice !== undefined) {
+    query.price = {};
+    if (filters.minPrice !== undefined) query.price.$gte = filters.minPrice;
+    if (filters.maxPrice !== undefined) query.price.$lte = filters.maxPrice;
+  }
+
+  if (filters.minRating !== undefined) {
+    query.rating = { $gte: filters.minRating };
+  }
+
+  if (filters.inStockDaily) {
+    query.stock = { $gt: 0 };
+  }
+
+  if (filters.tags && filters.tags.length > 0) {
+    query.tags = { $all: filters.tags } as any;
+  }
+
+  if (filters.search) {
+    query.name = { $regex: filters.search, $options: 'i' } as any;
+  }
+
+  console.log('Generated Query:', JSON.stringify(query, null, 2));
+
+  return await collection.find(query).toArray();
 }
